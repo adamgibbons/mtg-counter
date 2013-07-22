93fce863cdc9ceea35a8dfb7dd59ed51d186ebbb
adamgibbons <adam.d.gibbons@gmail.com>
2013-07-22 01:29:29 -0400

    adds game counter

--- app/players.js
+++ app/players.js
@@ -1,7 +1,13 @@
 function PlayersCtrl($scope) {
+  $scope.gameCount = 1;
+
+  incrementGameCount = function() {
+    $scope.gameCount += 1;
+  }
 
   $scope.reset = function() {
     $scope.$broadcast('reset');
+    incrementGameCount();
   }
 
 }
